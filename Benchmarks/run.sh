b2c78971f100417f3edf0296f59b115fee97b491
e5l <leonid.stashevsky@gmail.com>
2017-05-15 15:58:17 +0300

    rewrite benchmark scripts

--- Benchmarks/run.sh
+++ Benchmarks/run.sh
@@ -6,24 +6,15 @@ echo "SAVE [DONE]"
 
 path="tests/test_$filename.txt"
 
-echo "INDY: [START]"
-
-echo "INDY:" >> $path
-mvn -f indy_pom.xml clean install
-java -jar target/benchmarks.jar SingleRun.* -wi 20 -i 20 -f 1 -t 1 >> $path
-java -jar target/benchmarks.jar MultiTarget.* -wi 20 -i 20 -f 1 -t 1 >> $path
-java -jar target/benchmarks.jar MergeSort.* -wi 20 -i 20 -f 1 -t 1 >> $path
-
-echo "INDY: [DONE]"
-
-echo "" >> $path
-
-echo "REFLECTION: [START]"
-
-echo "REFLECTION:" >> $path
-mvn -f reflection_pom.xml clean install
-java -jar target/benchmarks.jar SingleRun.* -wi 20 -i 20 -f 1 -t 1 >> $path
-java -jar target/benchmarks.jar MultiTarget.* -wi 20 -i 20 -f 1 -t 1 >> $path
-java -jar target/benchmarks.jar MergeSort.* -wi 20 -i 20 -f 1 -t 1 >> $path
-
-echo "REFLECTION: [DONE]"
+SCRIPTS=./scripts/*.xml
+for file in $SCRIPTS
+do
+    echo "[START] $file" 
+    echo "[START] $file"  >> $path
+    mvn -f $file clean install
+    java -jar target/benchmarks.jar SingleRun.* -wi 20 -i 20 -f 1 -t 1 >> $path
+    java -jar target/benchmarks.jar MultiTarget.* -wi 20 -i 20 -f 1 -t 1 >> $path
+    java -jar target/benchmarks.jar MergeSort.* -wi 20 -i 20 -f 1 -t 1 >> $path
+    echo "[DONE] $file"  >> $path
+    echo "[DONE] $file"
+done
