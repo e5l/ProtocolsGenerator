db78fbb3e1eb9522381747a54bb73944e986331e
e5l <leonid.stashevsky@gmail.com>
2017-05-16 14:08:25 +0300

    benchmarks

--- Benchmarks/run.sh
+++ Benchmarks/run.sh
@@ -6,12 +6,12 @@ echo "SAVE [DONE]"
 
 path="tests/test_$filename.txt"
 
-SCRIPTS=./scripts/*.xml
+SCRIPTS=./*.xml
 for file in $SCRIPTS
 do
     echo "[START] $file" 
     echo "[START] $file"  >> $path
-    mvn -f $file clean install
+    mvn -q -f $file clean install
     java -jar target/benchmarks.jar SingleRun.* -wi 20 -i 20 -f 1 -t 1 >> $path
     java -jar target/benchmarks.jar MultiTarget.* -wi 20 -i 20 -f 1 -t 1 >> $path
     java -jar target/benchmarks.jar MergeSort.* -wi 20 -i 20 -f 1 -t 1 >> $path
